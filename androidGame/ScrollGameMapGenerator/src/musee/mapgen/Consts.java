12c6466c49d5fa61abe9445dcddbf1a57768d521
Lee Xiong <lx8@rice.edu>
2014-03-10 03:27:08 -0500

    generating perfect melody contour

--- androidGame/ScrollGameMapGenerator/src/musee/mapgen/Consts.java
+++ androidGame/ScrollGameMapGenerator/src/musee/mapgen/Consts.java
@@ -3,6 +3,7 @@ package musee.mapgen;
 public class Consts {
 	public static double sampleRatePerSecond = 6;
 	public static double tileToSampleRatio = 1;
+	public static int nonIndex = -1;
 
 	public static final double tileSpeed = 6.0; //6 per second	
 	public static final int tilesPerTone = 1;
